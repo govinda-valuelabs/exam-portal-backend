8da6040c5f834be082abdb9b5a65158530afc08e
Govinda Mandal <govinda.mandal@valuelabs.com>
2023-12-08 17:17:38 +0530

    Initialized with express app

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const corsOptions = {
+    origin: ['*']
+}
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to Exam Portal Backend!'});
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log('Server is listening at port ' + PORT);
+})
